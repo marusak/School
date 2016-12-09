d4ef668bcc5edb49d0e16b86f737598ac7ab9a51
Matej Marušák <marusak.matej@gmail.com>
2016-12-09 22:05:36 +0100

    IMP: Change file description from default

--- IMP/main.c
+++ IMP/main.c
@@ -1,47 +1,14 @@
-/*******************************************************************************
-   user_main: main for application using keyboard with interrupt
-   Copyright (C) 2009 Brno University of Technology,
-                      Faculty of Information Technology
-   Author(s): Jan Markovic <xmarko04 AT stud.fit.vutbr.cz>
-
-   LICENSE TERMS
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions
-   are met:
-   1. Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-   2. Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in
-      the documentation and/or other materials provided with the
-      distribution.
-   3. All advertising materials mentioning features or use of this software
-      or firmware must display the following acknowledgement:
-
-        This product includes software developed by the University of
-        Technology, Faculty of Information Technology, Brno and its
-        contributors.
-
-   4. Neither the name of the Company nor the names of its contributors
-      may be used to endorse or promote products derived from this
-      software without specific prior written permission.
-
-   This software or firmware is provided ``as is'', and any express or implied
-   warranties, including, but not limited to, the implied warranties of
-   merchantability and fitness for a particular purpose are disclaimed.
-   In no event shall the company or contributors be liable for any
-   direct, indirect, incidental, special, exemplary, or consequential
-   damages (including, but not limited to, procurement of substitute
-   goods or services; loss of use, data, or profits; or business
-   interruption) however caused and on any theory of liability, whether
-   in contract, strict liability, or tort (including negligence or
-   otherwise) arising in any way out of the use of this software, even
-   if advised of the possibility of such damage.
-
-   $Id$
-
-
-*******************************************************************************/
+/*
+ *  Vytah opat nechodi
+ *  main.c
+ *
+ *  Main module of Elevator controling written for FitKit.
+ *
+ *  Author: Matej Marusak, xmarus06@stud.fit.vutbr.cz
+ *
+ *  9.12.2016
+ *  Created as school project at VUT FIR Brno
+*/
 
 #include <fitkitlib.h>
 #include <stdio.h>
